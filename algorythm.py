1d83737912c6523696413d271b31252047949e04
Sakorni <ssakorni@gmail.com>
2022-09-25 01:52:51 +0300

    Second ver. Task 1.b and task 3 are done

--- algorythm.py
+++ algorythm.py
@@ -1,66 +1,193 @@
+import numpy as np
 import tkinter
-from PIL import ImageGrab
+from PIL import Image
+import math
+
+
+def _color_from_rgb(color):
+    """
+    _color_from_rgb convert color from RGB format into friendly to tkinter color
+    """
+    color = (color[0], color[1], color[2])
+    return "#%02x%02x%02x" % color
+
+# def componentToHex(color):
+#   var hex = c.toString(16); )))
+#   return hex.length == 1 ? "0" + hex : hex;
+# :
+
+# def rgbToHex(rgb):
+#     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 
 
 class Booba:
     canvas = None  # That's the current canvas
-    empty_color: tuple[int] = None  # in RGB format
-    fill_color: tuple[int]  # in RGB format
 
-    def __init__(self, canvas):
+    def __init__(self, canvas, canvas_color):
         self.canvas = canvas
-        self.empty_color = (255, 255, 255)
-
-    def _color_from_rgb(self):
-        """
-        _color_from_rgb convert color from RGB format into friendly to tkinter color
-        """
-        r, g, b = self.fill_color
-        return f'#{r:02x}{g:02x}{b:02x}'
-
-    def update_fill_color(self, color: tuple[int]):
-        self.fill_color = color
+        self.default_color = canvas_color
 
     def get_color(self, x, y):
-        image = ImageGrab.grab((x, y, x+1, y+1))  # 1 pixel image
-        return image.getpixel((0, 0))
+        arr = self.canvas.find_overlapping(x, y, x, y)
+        if (len(arr) == 0):
+            res = self.default_color
+        else:
+            res = self.canvas.itemcget(arr[-1], "fill")
+        return res
 
     def isEmptyPixel(self, x, y):
-        return self.get_pixel_color(x, y) == self.empty_color
+        if (x < 0 or x > self.canvas.winfo_reqwidth()):
+            return False
+        if (y < 0 or y > self.canvas.winfo_reqheight()):
+            return False
+        return self.get_color(x, y) == self.empty_color
 
     def drawLine(self, x1: int, y1: int, x2: int, y2: int):
         """
         drawLine draws a line from x1y1 point to x2y2 point
         """
         self.canvas.create_line(x1, y1, x2, y2,
-                                width=self.line_width, fill=self._color_from_rgb(),
-                                capstyle=tkinter.ROUND, smooth=tkinter.TRUE, splinesteps=36, tags=['line'])
-        print("Woah, line")
+                                fill=self.active_color[1], capstyle=tkinter.ROUND,
+                                smooth=tkinter.TRUE, splinesteps=36, tags=['drawn'])
+        #print("Woah, line")
 
     def findBorder(self, x: int, y: int, step: int):
         """
         findBorder returns x of last empty pixel
         """
-        while (self.isEmptyPixel(x+step, y)):
+        x += step
+        while (self.isEmptyPixel(x, y)):
             x += step
+        self.isEmptyPixel(x, y)
+        x -= step
+        return x
 
-    def fill(self, x: int, y: int, color: str):
-        # color from str
+    def fill(self, x: int, y: int, color: tuple):
+        self.active_color = color
         self.empty_color = self.get_color(x, y)
-        self._line_algorithm(x, y)
+        if (self.active_color[1] == self.empty_color):
+            return
+        self._line_algorithm(x, y, self)
         self.empty_color = None
 
-    def _line_algorithm(self, x: int, y: int):
+    def _line_algorithm(self, x: int, y: int, drawer):
         if (not self.isEmptyPixel(x, y)):
             return
         leftBound = self.findBorder(x, y, -1)
         rightBound = self.findBorder(x, y, 1)
-        self.drawLine(leftBound, y, rightBound, y)
+        drawer.drawLine(leftBound, y, rightBound + 1, y)
         i = leftBound
         while (i <= rightBound):
-            self._line_algorithm(i, y+1)
+            self._line_algorithm(i, y+1, drawer)
             i += 1
         i = leftBound
         while (i <= rightBound):
-            self._line_algorithm(i, y-1)
+            self._line_algorithm(i, y-1, drawer)
             i += 1
+
+    def set_pixel(self, x: int, y: int):
+        self.canvas.create_rectangle((x, y)*2, outline=self.active_color[1],
+                                     tags=['drawn', 'line_point'],
+                                     )
+
+    def _define_sign(self, d: int) -> int:
+        """
+        1 - сдвиг слева-направо или сверху-вниз
+        -1 - зеркальный сдвиг
+        """
+        if d > 0:
+            return 1
+        if d < 0:
+            return -1
+        return 0
+
+    def bresenham_line(self, color, x1=0, y1=0, x2=0, y2=0):
+        self.active_color = color
+        dx = x2 - x1  # смещение по x
+        dy = y2 - y1  # смещение по y
+
+        sign_x = self._define_sign(dx)
+        sign_y = self._define_sign(dy)
+
+        # эквивалент модулю. Мы уже знаем знаки, так что нам не надо их снова вычислять
+        dx *= sign_x
+        dy *= sign_y
+        dl = 0
+        dr = 0
+        x_addr1, x_addr2, y_addr1, y_addr2 = 0, 0, 0, 0
+        if dx == 0:
+            gradient = dy
+        else:
+            gradient = dy/dx
+
+        if gradient <= 1:
+            """
+            Если dx > dy, то значит отрезок "вытянут" вдоль оси икс, т.е. он скорее длинный, чем высокий.
+            Значит в цикле нужно будет идти по икс (строчка el = dx;), значит "протягивать" прямую по иксу
+            надо в соответствии с тем, слева направо и справа налево она идёт (pdx = incx;), при этом
+            по y сдвиг такой отсутствует.
+            """
+            dl, dr = dy, dx
+            x_addr2, y_addr1 = sign_x, sign_y
+            #pdx, pdy = sign_x, 0
+            last = dx
+        elif gradient > 1:
+            dl, dr = dx, dy
+            x_addr1, y_addr2 = sign_x, sign_y
+            #pdx, pdy = 0, sign_y
+            last = dy
+
+        di = 2*dl - dr
+        x, y = x1, y1
+        t = 0
+        self.set_pixel(x, y)  # рисуем первую точку вне цикла
+
+        while t < last:
+            if di < 0:
+                di += 2*dl
+            else:
+                y += y_addr1
+                x += x_addr1
+                di += 2*(dl-dr)
+            x += x_addr2
+            y += y_addr2
+            t += 1
+            self.set_pixel(x, y)
+
+    def _1b(self, x, y):
+        drawer = FancyDrawer("texture.jpg", self.canvas)
+        self.empty_color = self.get_color(x, y)
+        # if(self.active_color[1] == self.empty_color):
+        #     return
+        self._line_algorithm(x, y, drawer)
+        self.empty_color = None
+
+
+class FancyDrawer:
+    image: np.ndarray
+    _posx: int
+    _posy: int
+    canvas: tkinter.Canvas
+
+    def __init__(self, path: str, canv: tkinter.Canvas) -> None:
+        im = Image.open("./texture.jpg")
+        im.convert('RGB')
+        self.image = np.array(im)
+        self._posx = 0
+        self._posy = 0
+        self.canvas = canv
+
+    def drawLine(self, x1, y1, x2, y2):
+        while (x1 <= x2):
+            if self._posx >= len(self.image):
+                self._posx = 0
+                self._posy += 1
+            if self._posy >= len(self.image[self._posx]):
+                self._posy = 0
+            color = _color_from_rgb(self.image[self._posx][self._posy])
+            self.canvas.create_rectangle((x1, y1)*2, outline=color,
+                                         tags=['drawn'],
+                                         )
+            self._posx += 1
+            x1 += 1
+        print(y1)
--- main.py
+++ main.py
@@ -6,31 +6,44 @@ from algorythm import Booba
 class Paint(object):
 
     DEFAULT_PEN_SIZE = 5.0
-    DEFAULT_COLOR = 'black'
+    DEFAULT_PEN_COLOR = ((0.0, 0.0, 0.0), 'black')
+    DEFAULT_CANVAS_COLOR = ((1.0, 1.0, 1.0), 'white')
     W_SIZE = 600
     fill_mode = False
 
     def __init__(self):
         self.root = Tk()
 
-        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
+        self.pen_button = Button(self.root, text='pen', command=self.pen_mode)
         self.pen_button.grid(row=0, column=0)
+
+        self.fill_button = Button(
+            self.root, text='fill', command=self.fill_mode)
+        self.fill_button.grid(row=0, column=1)
+
+        self.fill_file_button = Button(
+            self.root, text='fill with img', command=self.fill_with_img_mode)
+        self.fill_file_button.grid(row=0, column=2)
+
+        self.line_button = Button(
+            self.root, text='line', command=self.line_mode)
+        self.line_button.grid(row=0, column=3)
 
         self.color_button = Button(
             self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=1)
+        self.color_button.grid(row=0, column=4)
 
         self.reset_button = Button(
-            self.root, text='reset', command=self.reset)
-        self.reset_button.grid(row=0, column=2)
+            self.root, text='clear', command=self.clear_canvas)
+        self.reset_button.grid(row=0, column=5)
 
         self.choose_size_button = Scale(
             self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=3)
+        self.choose_size_button.grid(row=0, column=6)
 
         self.c = Canvas(self.root, bg='white',
                         width=self.W_SIZE, height=self.W_SIZE)
-        self.c.grid(row=1, columnspan=4)
+        self.c.grid(row=1, columnspan=7)
 
         self.setup()
         self.root.mainloop()
@@ -39,41 +52,94 @@ class Paint(object):
         self.old_x = None
         self.old_y = None
         self.line_width = self.choose_size_button.get()
-        self.color = 'black'
+        self.color = self.DEFAULT_PEN_COLOR
         self.active_button = self.pen_button
-        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<B1-Motion>', self.on_motion)
+        self.c.bind('<ButtonPress-1>', self.on_press)
         self.c.bind('<ButtonRelease-1>', self.reset)
-        self.filler = Booba(self.c)
+        self.filler = Booba(self.c, self.DEFAULT_CANVAS_COLOR[1])
+        self.mode = None
 
-    def use_pen(self):
+    def pen_mode(self):
         self.activate_button(self.pen_button)
+        self.mode = 0
+
+    def fill_mode(self):
+        self.activate_button(self.fill_button)
+        self.mode = 1
+
+    def fill_with_img_mode(self):
+        self.activate_button(self.fill_file_button)
+        self.mode = 2
+
+    def line_mode(self):
+        self.activate_button(self.line_button)
+        self.mode = 3
 
     def choose_color(self):
-        whoAsked = askcolor(color=self.color)
-        self.filling_color = whoAsked[0]
-        self.fill_mode = True
+        self.color = askcolor(color=self.color[1])
 
     def activate_button(self, btn_to_act):
         self.active_button.config(relief=RAISED)
         btn_to_act.config(relief=SUNKEN)
         self.active_button = btn_to_act
 
-    def paint(self, event):
-        if (self.fill_mode):
-            self.filler.fill(event.x, event.y, self.filling_color)
-            self.fill_mode = False
-        self.line_width = self.choose_size_button.get()
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
-                               width=self.line_width, fill=self.color,
-                               capstyle=ROUND, smooth=TRUE, splinesteps=36, tags=['line'])
+    def on_motion(self, event):
+        if self.mode == 0:
+            self.line_width = self.choose_size_button.get()
+            if self.old_x and self.old_y:
+                self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                                   width=self.line_width, fill=self.color[1],
+                                   capstyle=ROUND, smooth=TRUE, splinesteps=36, tags=['drawn'])
 
-        self.old_x = event.x
-        self.old_y = event.y
+            self.old_x = event.x
+            self.old_y = event.y
+
+    def on_press(self, event):
+        if self.mode == 1:
+            self.filler.fill(event.x, event.y, self.color)
+            return
+        if self.mode == 2:
+            self.filler._1b(event.x, event.y)
+            return
+
+        if self.mode == 3:
+            points = self.c.find_withtag("point")
+            if len(points) == 0:
+                self.c.create_oval(event.x, event.y, event.x + 5.0, event.y + 5.0,
+                                   fill=self.color[1], tags=['drawn', 'point'])
+                return
+            if len(points) == 2:
+                self.c.delete(points[0])
+                self.c.delete("line_point")
+
+            self.c.create_oval(event.x, event.y, event.x + 5.0, event.y + 5.0,
+                               fill=self.color[1], tags=['drawn', 'point'])
+            points = self.c.find_withtag("point")
+            xy1 = self.c.coords(points[0])
+            xy2 = self.c.coords(points[1])
+            self.filler.bresenham_line(
+                self.color, xy1[0], xy1[1], xy2[0], xy2[1])
+
+            return
 
     def reset(self, event):
         self.old_x, self.old_y = None, None
-        self.c.delete('line')
+
+    def clear_canvas(self):
+        self.c.delete('drawn')
+
+    def draw_test_square(self):
+        self.c.create_line(20, 20, 80, 20)
+        self.c.create_line(20, 20, 20, 80)
+        self.c.create_line(20, 80, 80, 80)
+        self.c.create_line(80, 20, 80, 80)
+
+    def draw_test_square(self):
+        self.c.create_line(20, 20, 80, 20)
+        self.c.create_line(20, 20, 20, 80)
+        self.c.create_line(20, 80, 80, 80)
+        self.c.create_line(80, 20, 80, 80)
 
 
 if __name__ == '__main__':
